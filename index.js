f6292575eae312a10c053f9c63959d75368712b9
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-03-15 16:05:14 +0200

    Receiving data

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+// const requre 
 
 app.use(express.json())
+
+// morgan.token('body', (req) => JSON.stringify(req.body));
+
+// app.use(morgan(':url :method :body'))
 
 let persons = [
   {
